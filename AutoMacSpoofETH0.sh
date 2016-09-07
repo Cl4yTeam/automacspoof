a5f12588cc377a22a2373de572e6a489681ae8c2
root <root@localhost.localdomain>
2016-09-07 06:03:15 +0200

    first commit

---
+++ AutoMacSpoofETH0.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+clear
+echo " ##############################################################################"
+echo " #                                                                            #"
+echo " #                 .d8888b.  888             d8888 Y88b   d88P                #"
+echo " #                d88P  Y88b 888            d88888  Y88b d88P                 #"
+echo " #                888    888 888           d88P888   Y88o88P                  #"
+echo " #                888        888          d88P 888    Y888P                   #"
+echo " #                888        888         d88P  888     888                    #"
+echo " #                888    888 888        d88P   888     888                    #"
+echo " #                Y88b  d88P 888       d8888888888     888                    #"
+echo " #                 'Y8888P'  88888888 d88P     888     888 (eth0)             #"
+echo " #                                                                            #"
+echo " ##############################################################################"
+sleep 1
+echo "Welcome To Auto Mac Address Spoof - Clay Team (C) 2016 clayteamhack@gmail.com"
+sleep 3.5
+echo ""
+echo "For Stop Script: Ctrl + C"
+sleep 2
+echo ""
+echo "Youtube - https://youtube.com/channel/UCjBOxyh9umG4xSGifbqB_FA"
+echo "Twitter - https://twitter.com/Cl4yTeam"
+echo "Telegram - https://telegram.com/ClayTeam"
+echo "Github - https://github.com/Cl4yTeam"
+echo ""
+sleep 2
+echo "Enjoy Auto Spoofer ;)"
+echo ""
+echo ""
+sleep 1
+while true; do
+    sleep 1
+    macchanger -r eth0
+done
